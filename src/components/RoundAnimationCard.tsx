3875ec047270f16c1447d0f8aff2b35f8136792a
vivekkum-ar <vivekkr9119@gmail.com>
2024-03-28 10:11:46 +0530

    Feat: Testimonials carousel added

--- src/components/RoundAnimationCard.tsx
+++ src/components/RoundAnimationCard.tsx
@@ -15,13 +15,12 @@ interface RoundAnimationCardProps {
 const RoundAnimationCard: React.FC<RoundAnimationCardProps> = ({ icon,title,description,imageUrl,classes,animId }) => {
     return (
         <div className={`w-fit min-h-fit flex ${classes}`} id={animId}>
-            <div className='relative flex flex-row h-56 rounded-2xl outline w-96' style={{filter:"blur(2px)", backgroundImage: `url("${imageUrl}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+            <div className="relative flex flex-row h-56 rounded-2xl w-96 before:w-full before:h-full before:rounded-2xl before:bg-slate-900 before:opacity-50 before:absolute before:block transition duration-150 ease-linear" style={{ backgroundImage: `url("${imageUrl}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
             <h2 className='text-white absolute font-psemibold text-xl px-6 py-4 text-justify ms-2'>
                 <span className="mx-6"><Icon icon={icon ? icon : "mdi:star-four-points-outline"} className='absolute text-white' width={20} height={20}></Icon>
             <Icon icon="mdi:star-four-points-outline" className='absolute text-white' width={20} height={20}></Icon>
                     {title}</span><br /><span className='text-lg font-pregular'>{description}</span></h2>
         </div>
-    )
-}
+)}
 
 export default RoundAnimationCard
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,8 +20,12 @@ import AnimatedCard from '@/components/AnimatedCard';
 import { setTimeout } from 'timers/promises';
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import RoundAnimationCard from '@/components/RoundAnimationCard';
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
 // import { WebConnect } from '@/assets/lottie/VUaU1Z4EFW.lottie';
-
+import Autoplay from "embla-carousel-autoplay"
+import { Bird } from 'lucide-react';
+import { Icon } from '@iconify/react/dist/iconify.js';
 interface HomeProps {
   // Add your prop types here
   // theme: string;
@@ -33,113 +37,200 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Home: React.FC<HomeProps> = ({ }) => {
 
+  const cardsData = [
+    {
+      title: "Connect on Social Media",
+      description: "Add your links to Twitter, Facebook, and Instagram in one place for easy access."
+    },
+    {
+      title: "Your Personalized QR Code",
+      description: "Generate a unique QR code to instantly share your social profiles and contact info."
+    },
+    {
+      title: "Seamless Profile Sharing",
+      description: "Allow others to discover all your important links with just one scan."
+    },
+    {
+      title: "Collate and Curate",
+      description: "Create a personalized collection of links, bios, and profiles with ease."
+    },
+    {
+      title: "Boost Your Visibility",
+      description: "Get noticed and make sharing your digital identity hassle-free."
+    },
+    {
+      title: "Dynamic and Stylish",
+      description: "Showcase your online presence in a visually appealing and dynamic format."
+    },
+    {
+      title: "Share with One Tap",
+      description: "Simplify how you share your bio and social links with one powerful card."
+    },
+    {
+      title: "Your Digital Footprint",
+      description: "Combine your professional and personal links in a single, sleek profile."
+    },
+    {
+      title: "Tailored for Everyone",
+      description: "Whether you're a creator, business, or influencer, this app is for you."
+    }
+  ];
   const [count, setCount] = useState<number>(0);
 
   useEffect(() => {
     window.setTimeout(() => {
-      setCount(prev => prev >= 2 ? 0 : prev+1);
+      setCount(prev => prev >= 2 ? 0 : prev + 1);
     }, 1500);
-  },[count])
+  }, [count])
 
-  useGSAP(() => {
-    // var tl = anime.timeline({
-    //   easing: 'easeOutExpo',
-    //   duration: 750,
-    //   autoplay:false
-    // });
-    var tl = anime.timeline({
-      easing: 'easeOutSine',
-      duration: 750,
-      autoplay:false
-    });
-    tl.add({
-      targets:"#img1",
-      translateX:5,
-      translateY:-250
-    })
-    tl.add({
-      targets:"#img8",
-      translateX:430,
-      translateY:-250
-    })
-    tl.add({
-      targets:"#img7",
-      translateX:430,
-      translateY:30
-    })
-    tl.add({
-      targets:"#img6",
-      translateX:430,
-      translateY:310
-    })
-    tl.add({
-      targets:"#img5",
-      translateX:5,
-      translateY:310
-    })
-    tl.add({
-      targets:"#img4",
-      translateX:-420,
-      translateY:310
-    })
-    tl.add({
-      targets:"#img3",
-      translateX:-420,
-      translateY:30
-    })
-    tl.add({
-      targets:"#img2",
-      translateX:-420,
-      translateY:-250
-    })
-    tl.add({
-      targets:"#img0",
-      scale:1.5,
-    })
-    tl.add({
-      targets:['#img1','#img2','#img3','#img4','#img5','#img6','#img7','#img8'],
-      rotate:() => anime.random(-10,10),
-    })
-    gsap.from(".boxa", {
-      scrollTrigger: {
-        trigger: ".box-container",
-        start: "top top+=10%",  // [trigger] [scroller] positions
-        end: "bottom+=200% 80%", // [trigger] [scroller] positions
-        // or relative amount: "+=500"
-        scrub: true, // or time (in seconds) to catch up
-        pin: true, // or selector or element to pin
-        markers: true, // only during development!
+    const testimonialsData = [
+      {
+        text: "This app made sharing my social profiles so much easier. Highly recommended!",
+        number: 5,
+        url: "./images/user1.jpg"
       },
-      x: "110%",
-      stagger: {
-        amount:1,
-        from:"start",
+      {
+        text: "I love how professional my QR code looks. It’s a game-changer for my business.",
+        number: 4,
+        url: "./images/user2.jpg"
       },
-    });
-    gsap.from(".round-card-target", {
-      scrollTrigger: {
-        trigger: ".round-card-trigger",
-        start: "top top",  // [trigger] [scroller] positions
-        end: "bottom+=200% center-=20%", // [trigger] [scroller] positions
-        // or relative amount: "+=500"
-        scrub: -1000, // or time (in seconds) to catch up
-        // pin: true, // or selector or element to pin
+      {
+        text: "A must-have app for creators and influencers. Simplifies everything!",
+        number: 3,
+        url: "./images/user3.jpg"
+      },
+      {
+        text: "The dynamic design and simplicity make it stand out from other apps.",
+        number: 3.7,
+        url: "./images/user4.jpg"
+      },
+      {
+        text: "Having all my links in one place has increased my online engagement significantly.",
+        number: 4,
+        url: "./images/user5.jpg"
+      },
+      {
+        text: "I can’t believe how easy it is to share my entire profile with a single QR code.",
+        number: 5,
+        url: "./images/user6.jpg"
+      },
+      {
+        text: "This app saves so much time and makes sharing effortless. Highly intuitive!",
+        number: 5,
+        url: "./images/user7.jpg"
+      },
+      {
+        text: "The perfect tool for professionals to boost their digital presence effectively.",
+        number: 5,
+        url: "./images/user8.jpg"
+      },
+      {
+        text: "The customization options are fantastic. Love the polished feel it provides!",
+        number: 4,
+        url: "./images/user9.jpg"
+      }
+    ];
+  
+  
+  // useGSAP(() => {
+  //   // var tl = anime.timeline({
+  //   //   easing: 'easeOutExpo',
+  //   //   duration: 750,
+  //   //   autoplay:false
+  //   // });
+  //   var tl = anime.timeline({
+  //     easing: 'easeOutSine',
+  //     duration: 750,
+  //     autoplay: false
+  //   });
+  //   tl.add({
+  //     targets: "#img1",
+  //     translateX: 5,
+  //     translateY: -250
+  //   })
+  //   tl.add({
+  //     targets: "#img8",
+  //     translateX: 430,
+  //     translateY: -250
+  //   })
+  //   tl.add({
+  //     targets: "#img7",
+  //     translateX: 430,
+  //     translateY: 30
+  //   })
+  //   tl.add({
+  //     targets: "#img6",
+  //     translateX: 430,
+  //     translateY: 310
+  //   })
+  //   tl.add({
+  //     targets: "#img5",
+  //     translateX: 5,
+  //     translateY: 310
+  //   })
+  //   tl.add({
+  //     targets: "#img4",
+  //     translateX: -420,
+  //     translateY: 310
+  //   })
+  //   tl.add({
+  //     targets: "#img3",
+  //     translateX: -420,
+  //     translateY: 30
+  //   })
+  //   tl.add({
+  //     targets: "#img2",
+  //     translateX: -420,
+  //     translateY: -250
+  //   })
+  //   tl.add({
+  //     targets: "#img0",
+  //     scale: 1.5,
+  //   })
+  //   tl.add({
+  //     targets: ['#img1', '#img2', '#img3', '#img4', '#img5', '#img6', '#img7', '#img8'],
+  //     rotate: () => anime.random(-10, 10),
+  //   })
+  //   gsap.from(".boxa", {
+  //     scrollTrigger: {
+  //       trigger: ".box-container",
+  //       start: "top top+=10%",  // [trigger] [scroller] positions
+  //       end: "bottom+=200% 80%", // [trigger] [scroller] positions
+  //       // or relative amount: "+=500"
+  //       scrub: true, // or time (in seconds) to catch up
+  //       pin: true, // or selector or element to pin
+  //       markers: true, // only during development!
+  //     },
+  //     x: "110%",
+  //     stagger: {
+  //       amount: 1,
+  //       from: "start",
+  //     },
+  //   });
+  //   gsap.from(".round-card-target", {
+  //     scrollTrigger: {
+  //       trigger: ".round-card-trigger",
+  //       start: "top top",  // [trigger] [scroller] positions
+  //       end: "bottom+=200% center-=20%", // [trigger] [scroller] positions
+  //       // or relative amount: "+=500"
+  //       scrub: -1000, // or time (in seconds) to catch up
+  //       // pin: true, // or selector or element to pin
 
-        markers: true, // only during development!
-        onUpdate:(scroll:any) => {
-          console.log("progress: ",scroll.progress,"tl.progress: ",(scroll.progress * 100));
-          tl.seek(tl.duration * scroll.progress);
-        }
-      },
-      // x: "110%",
-      // stagger: {
-      //   amount: 1,
-      //   from: "start"
-      // },
-    }); // <-- automatically reverted
-  },
-  // { scope: container }
-) // <-- scope is for selector text (optional)
+  //       markers: true, // only during development!
+  //       onUpdate: (scroll: any) => {
+  //         // console.log("progress: ", scroll.pyrogress, "tl.progress: ", (scroll.progress * 100));
+  //         tl.seek(tl.duration * scroll.progress);
+  //       }
+  //     },
+  //     // x: "110%",
+  //     // stagger: {
+  //     //   amount: 1,
+  //     //   from: "start"
+  //     // },
+  //   }); // <-- automatically reverted
+  // },
+  //   // { scope: container }
+  // ) // <-- scope is for selector text (optional)
 
   const customTracker = (detectedCodes: IDetectedBarcode[], ctx: CanvasRenderingContext2D): void => {
     // Clear the canvas
@@ -165,9 +256,9 @@ const Home: React.FC<HomeProps> = ({ }) => {
   const [pause, setPause] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='w-full h-screen flex flex-col items-center'>
+    <div className='w-full h-screen flex flex-col items-center dark:bg-zinc-950'>
       <Jumbotron />
-       <div className="w-full px-4 md:px-16 py-2 md:py-8 dark:bg-zinc-950 relative">
+      <div className="w-full px-4 md:px-16 py-2 md:py-8 dark:bg-zinc-950 relative">
         <div className="outline-1 outline outline-red-500 relative h-[30rem] box-container" >
           <AnimatedCard id={0} title='Customizable QR Codes' description='Unify your digital presence with a single, scannable QR code.'
             features={[
@@ -272,44 +363,76 @@ const Home: React.FC<HomeProps> = ({ }) => {
             ]
           } lottieSource={sharingHeart}></AnimatedCard>
         </div>
-      </div>  
- <div className="font-pregular text-center w-screen pt-12 pb-4 ">
-        <h1 className="mb-4 text-3xl font-semibold tracking-tight round-card-trigger leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Add your links to your&nbsp; 
+      </div>
+      <div className="font-pregular text-center w-screen pt-12">
+        <h1 className="mb-4 text-3xl font-semibold tracking-tight round-card-trigger leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Add your links to your&nbsp;
           <div className='inline-flex min-w-[5em]'>
-        <p className={`${count == 0 ? " animate__fast animate__fadeInDown" : " animate__fast animate__fadeOutDown hidden"} animate__animated font-mbold inline-flex pb-3 bg-gradient-to-br from-violet-700 to-fuchsia-700 bg-clip-text text-transparent`}>Instagram </p>
-    <p className={`${count == 1 ? " animate__fast animate__fadeInDown" : " animate__fast animate__fadeOutDown hidden"} animate__animated font-mbold inline-flex pb-3 bg-gradient-to-br from-blue-700 to-teal-900 bg-clip-text text-transparent`}>Facebook </p>
-    <p className={`${count == 2 ? " animate__fast animate__fadeInDown" : " animate__fast animate__fadeOutDown hidden"} animate__animated font-mbold inline-flex pb-3 bg-gradient-to-br from-gray-700 via-gray-200 to-gray-500 bg-clip-text text-transparent`}>Twitter </p>
+            <p className={`${count == 0 ? " animate__fast animate__fadeInDown" : " animate__fast animate__fadeOutDown hidden"} animate__animated font-mbold inline-flex pb-3 bg-gradient-to-br from-violet-700 to-fuchsia-700 bg-clip-text text-transparent`}>Instagram </p>
+            <p className={`${count == 1 ? " animate__fast animate__fadeInDown" : " animate__fast animate__fadeOutDown hidden"} animate__animated font-mbold inline-flex pb-3 bg-gradient-to-br from-blue-700 to-teal-900 bg-clip-text text-transparent`}>Facebook </p>
+            <p className={`${count == 2 ? " animate__fast animate__fadeInDown" : " animate__fast animate__fadeOutDown hidden"} animate__animated font-mbold inline-flex pb-3 bg-gradient-to-br from-gray-700 via-gray-200 to-gray-500 bg-clip-text text-transparent`}>Twitter </p>
           </div>
-          <br/> and other bio sections
-          </h1>
-          <div className="relative w-full p-12 flex flex-row items-center justify-center round-card-target h-[130vh]">
-      <RoundAnimationCard classes='absolute z-10' animId={'img0'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/1.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img1'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/2.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img2'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/3.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img3'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/4.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img4'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/5.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img5'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/6.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img6'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/7.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img7'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/8.jpg'}></RoundAnimationCard>
-      <RoundAnimationCard classes='absolute' animId={'img8'} title={'Lorem Ipsum.'} description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi atque ipsum provident fuga, ratione reiciendis fugit tempore nostrum tempora.'} imageUrl={'./images/9.jpg'}></RoundAnimationCard>
+          <br /> and other bio sections
+        </h1>
+        <div className="relative bg-zinc-950 w-full p-12 flex flex-row items-center justify-center round-card-target h-[130vh]">
+
+          {
+            cardsData.map((items, index) => {
+              console.log(index);
+              return (
+                <RoundAnimationCard
+                  key={index}
+                  classes={`absolute bg-zinc-950 img${index} ${index == 0 ? "z-10" : "z-0"}`}
+                  animId={`img${index}`}
+                  title={items.title}
+                  description={items.description}
+                  imageUrl={`./images/${index+1}.jpg`}>
+                </RoundAnimationCard>
+              )
+            })
+          }
+          
+        </div>
       </div>
+      <div className="bg-zinc-950 w-screen h-full flex flex-row justify-center">
+      <Carousel className="w-full max-w-5xl px-12 bg-zinc-950">
+      <CarouselContent>
+       {
+        testimonialsData.map((item,index) => (
+          <CarouselItem key={index} className='md:basis-1/2 lg:basis-1/3'>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center p-0 justify-center  dark:bg-zinc-950">
+                  {/* <span className="text-4xl font-semibold">{index + 1}</span> */}
+                  <div className="font-pmedium text-justify items-center flex flex-col p-2 w-full h-full bg-zinc-100 dark:bg-zinc-700" style={{clipPath:"polygon(100% 0, 100% 100%,50% 75%, 0 100%, 0 0)"}}>
+                  {item.text}
+                  <div className='flex flex-row pt-2'>
+                    {
+                      Array.from({length:Math.floor(item.number)}).map((_,index) => (
+                        <Icon icon={"fa-star"} className='text-amber-500'></Icon>
+                      ))
+                   }
+                    {
+                      Array.from({length:5 - Math.floor(item.number)}).map((_,index) => (
+                        <Icon icon={"fa-regular:star"} className='text-amber-500'></Icon>
+                      ))
+                   }
+                  </div>
+                  </div>
+                    <img src="./images/1.jpg" alt="" className='rounded-full bottom-[2.0rem] absolute h-24 w-24 aspect-square' />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))
+       }
+
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
       </div>
-      
- {/* <div className="relative w-full p-12 flex flex-row items-center justify-items-center h-auto round-card-target">
-  <div style={{backgroundImage:"url('./images/ph.jpg')", backgroundRepeat:"no-repeat", backgroundSize:"cover"}} className='w-[25%] h-[44] img1'>111111</div>
-  <AspectRatio ratio={16 / 9} className='relative flex flex-row items-center justify-center'>
-    <img src = "./images/9.jpg" alt='img0' id="img0" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/1.jpg" alt='img1' id="img1" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/2.jpg" alt='img2' id="img2" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/3.jpg" alt='img3' id="img3" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/10.jpg" alt='img4' id="img4" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/5.jpg" alt='img5' id="img5" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/6.jpg" alt='img6' id="img6" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/7.jpg" alt='img7' id="img7" className="rounded-2xl absolute object-cover h-64"/>
-    <img src = "./images/8.jpg" alt='img8' id="img8" className="rounded-2xl absolute object-cover h-64"/>
-  </AspectRatio>
- </div> */}
-      </div>
+ 
+    </div>
 
   )
 }
