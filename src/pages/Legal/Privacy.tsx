342401a092ce66f5f80ef155890619bcb277960b
vivekkum-ar <vivekkr9119@gmail.com>
2024-04-09 16:11:17 +0530

    Chore: Added Privacy policy page

--- src/pages/Legal/Privacy.tsx
+++ src/pages/Legal/Privacy.tsx
@@ -1,7 +1,22 @@
-const Privacy = () => {
+import Markdown from 'markdown-to-jsx';
+import { useState, useEffect } from 'react';
+
+const Terms = () => {
+  
+      const [content, setContent] = useState("");
+    
+      useEffect(() => {
+        fetch("/mds/privacy.md") // Fetch the file from the public folder
+          .then((response) => response.text())
+          .then((text) => setContent(text))
+          .catch((error) => console.error("Error loading Markdown:", error));
+      }, []);
+
   return (
-    <div>Privacy</div>
+    <Markdown className="dark:text-white max-w-screen-lg mx-auto text-zinc-600 font-pmedium text-justify md:text-prose prose-sm md:prose-lg disable-tailwind">
+    {content}
+  </Markdown>
   )
 }
 
-export default Privacy
+export default Terms
