12d11e3ab6a00dde2715c95deb494b3f40fc0493
vivekkum-ar <vivekkr9119@gmail.com>
2024-04-10 19:22:17 +0530

    Feat: Breadcumb now can automatically detect links

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 interface BreadcrumbItem {
     label: string
@@ -11,23 +11,31 @@ interface BreadcrumbProps {
 }
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ items }) => {
+    const location = useLocation()
 
     return (
         <nav className="dark:text-white text-white absolute z-10 bottom-0 left-4 hidden md:block" aria-label="Breadcrumb">
-            <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <ol className="inline-flex items-center space-x-0.5 rtl:space-x-reverse">
                 {
-                    items.map((item, index) => (
+                    location.pathname.split("/").map((item, index) => (
                         <>
+                        {index == 0 &&
                             <li className="inline-flex items-center">
-                                <Link to={`/` + item.label} className="inline-flex items-center text-sm font-medium text-white hover:text-blue-600 dark:hover:text-white">
-                                    <Icon icon={item.icon} className="h-5 w-5"></Icon>
-                                    {item.label}
+                                <Link to={"/"} className="text-white hover:text-blue-600 dark:hover:text-white">
+                                    <Icon icon="ic:round-home" className="h-6 w-6"></Icon>
+                                </Link>
+                            </li>
+                        }
+                            <li className="inline-flex items-center">
+                                <Link to={location.pathname.split("/").splice(0,index+1).join("/")} className="inline-flex items-center text-sm font-medium text-white hover:text-blue-600 dark:hover:text-white">
+                                    <Icon icon={item} className="h-5 w-5"></Icon>
+                                    {item}
                                 </Link>
                             </li>
                             {
                                 (index + 1 != items.length) ? 
                                 <div className="flex items-center text-white">
-                                    <Icon icon="line-md:chevron-right" className="w-5 h-5"></Icon>
+                                    <Icon icon="line-md:chevron-right" className=" h-5"></Icon>
                                 </div> : ""
                             }
 
